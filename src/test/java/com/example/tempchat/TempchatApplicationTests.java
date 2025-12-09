acbe241cdbf5638ed13ecc0d3e506f31000767a8
Jimmy <gmi483483@gmail.com>
2025-12-09 16:55:29 +0545

    First Commit

---
+++ src/test/java/com/example/tempchat/TempchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tempchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
