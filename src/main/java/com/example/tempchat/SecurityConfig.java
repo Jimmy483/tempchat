fab38d2f538ed1ef34cdef43707b48b9fcb86ae0
Jimmy <gmi483483@gmail.com>
2026-02-25 18:08:57 +0545

    implementation of csrf handling. Issue with unable to remove the default spring security login page.

---
+++ src/main/java/com/example/tempchat/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.tempchat;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity
+                .csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().permitAll() // Allow all requests without authentication
+        );
+//        httpSecurity
+//                .csrf(csrf->csrf
+//                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+//                .authorizeHttpRequests(auth->auth
+//                        .anyRequest().permitAll())
+//                .formLogin(form->form.disable())
+//                .logout(logout->logout.logoutUrl("/menu/logout").permitAll());
+        return httpSecurity.build();
+    }
+}
